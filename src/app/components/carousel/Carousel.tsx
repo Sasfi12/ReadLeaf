b4c3d694e671381097ff6c34be75a47f8f5b806c
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2026-02-01 14:02:33 +0100

    solution: map the randomized_data array into a new array that contains id, could be done upstream too, but to respect my weird -2 ~ 2 index system I'll do it downstream

--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -4,39 +4,46 @@ import Link from "next/link";
 import { Book } from "@/lib/apiTypes";
 export default function Carousel({randomized_data} : {randomized_data : Book[]}) { 
     const [currentIndex , setCurrentIndex] = useState(0);
-    const prevElem = () => {
-        
-        if(currentIndex == 2) setCurrentIndex(() => -2) 
-        else if(currentIndex !== 2) setCurrentIndex((PreviousIndex) => PreviousIndex + 1)
+    const [data , setCurrentData] = useState(); 
+    if (randomized_data) {
+            for(let i = 0 ; i < randomized_data.length ; i++ ) {
+
+            }
+
+        const prevElem = () => {
+
+            if(currentIndex == 2) setCurrentIndex(() => -2) 
+            else if(currentIndex !== 2) setCurrentIndex((PreviousIndex) => PreviousIndex + 1)
+        }
+        const nextElem = () => {
+
+            if(currentIndex !== -2) setCurrentIndex((PreviousIndex) => PreviousIndex - 1)
+            else if(currentIndex == -2) setCurrentIndex((PreviousIndex) => 2)
+        }
+        useEffect(() => {
+           let carouselInterval : NodeJS.Timeout = setInterval(()=>  nextElem(), 6000)
+            return () => clearInterval(carouselInterval)
+        },[])
+        useEffect(() => {
+            if(currentIndex == -2) setTimeout(() =>{if(currentIndex == -2) setCurrentIndex(2)} , 6000)
+            
+        } , [currentIndex])
+        return (
+            <section className="carousel-section">
+                <button className="previous-button" onClick={() => prevElem()}>Previous</button>
+                <div className="carousel-container">
+
+                        <div className="carousel-items" style={{width: ""}}>
+                                    <div className="carousel-item" id={`index${randomized_data[currentIndex].id}`} key={randomized_data[currentIndex].id}>
+                                        <h1 >{randomized_data[currentIndex].title}</h1>
+                                        <img src={randomized_data[currentIndex].image_url} alt={`${randomized_data[currentIndex].title} image`} />
+                                        <Link href={`products/${randomized_data[currentIndex].id}`}>See more</Link>
+                                    </div>
+                        </div> 
+
+                </div>
+                <button className="next-button" onClick={() => nextElem()}>Next</button>
+            </section>
+        )
     }
-    const nextElem = () => {
-        
-        if(currentIndex !== -2) setCurrentIndex((PreviousIndex) => PreviousIndex - 1)
-        else if(currentIndex == -2) setCurrentIndex((PreviousIndex) => 2)
-    }
-    useEffect(() => {
-       let carouselInterval : NodeJS.Timeout = setInterval(()=>  nextElem(), 6000)
-        return () => clearInterval(carouselInterval)
-    },[])
-    useEffect(() => {
-        if(currentIndex == -2) setTimeout(() =>{if(currentIndex == -2) setCurrentIndex(2)} , 6000)
- 
-    } , [currentIndex])
-    return (
-        <section className="carousel-section">
-            <button className="previous-button" onClick={() => prevElem()}>Previous</button>
-            <div className="carousel-container">
-                   
-                    <div className="carousel-items" style={{width: ""}}>
-                                <div className="carousel-item" id={`index${randomized_data[currentIndex].id}`} key={randomized_data[currentIndex].id}>
-                                    <h1 >{randomized_data[currentIndex].title}</h1>
-                                    <img src={randomized_data[currentIndex].image_url} alt={`${randomized_data[currentIndex].title} image`} />
-                                    <Link href={`products/${randomized_data[currentIndex].id}`}>See more</Link>
-                                </div>
-                    </div> 
-                    
-            </div>
-            <button className="next-button" onClick={() => nextElem()}>Next</button>
-        </section>
-    )
 }
