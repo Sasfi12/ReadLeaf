f68604e576be29d4078780c7c37472a744437f5d
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2026-02-01 13:44:43 +0100

    bug: this causes a crash

--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -28,10 +28,10 @@ export default function Carousel({randomized_data} : {randomized_data : Book[]})
             <div className="carousel-container">
                    
                     <div className="carousel-items" style={{width: ""}}>
-                                <div className="carousel-item" id={`index${e.id}`} key={e.id}>
-                                    <h1 >{e.title}</h1>
-                                    <img src={e.image_url} alt={`${e.title} image`} />
-                                    <Link href={`products/${e.id}`}>See more</Link>
+                                <div className="carousel-item" id={`index${randomized_data[currentIndex].id}`} key={randomized_data[currentIndex].id}>
+                                    <h1 >{randomized_data[currentIndex].title}</h1>
+                                    <img src={randomized_data[currentIndex].image_url} alt={`${randomized_data[currentIndex].title} image`} />
+                                    <Link href={`products/${randomized_data[currentIndex].id}`}>See more</Link>
                                 </div>
                     </div> 
                     
